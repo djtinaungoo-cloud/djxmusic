5c97ea67579bb1a6712eedcfbd9972b2f2508226
djtinaungoo-cloud <djtinaungoo@gmail.com>
2026-05-24 19:30:11 +0630

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/djx-mix-player/',
+  base: '/djxmusic/',
 }
   try {
     // @ts-ignore
